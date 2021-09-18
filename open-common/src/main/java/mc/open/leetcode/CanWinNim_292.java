4dbda9096426edc234813a24f2d6e1952f105708
macheng <hk@123456>
2021-09-18 16:39:38 +0800

    leetcode 292

---
+++ open-common/src/main/java/mc/open/leetcode/CanWinNim_292.java
@@ -0,0 +1,13 @@
+package mc.open.leetcode;
+
+/**
+ * @author macheng
+ * @date 2021/9/18 16:38
+ */
+public class CanWinNim_292 {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+}
